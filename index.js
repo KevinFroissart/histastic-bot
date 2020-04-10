5870d3d3c21c08f47505d4a44f140ad064742dfd
Froissart Kevin <kevin.froissart69@gmail.com>
2020-04-10 15:47:50 +0200

    getting started

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+require('dotenv').config();
 const Discord = require('discord.js');
-const client = new Discord.client();
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`This bot is online!`);
+})
+
+client.on('message', msg=>{
+    if(msg.content === "salut"){
+        msg.reply('raconte pas ta vie fils de pute');
+    }
+})
+
+client.login(process.env.BOT_TOKEN);
